cb69d89d0538979b254872f4136ba7929008648d
SwifterLC <948058291@qq.com>
2017-08-02 04:25:57 +0800

    删除低内存警告

--- Cheng/ViewController.swift
+++ Cheng/ViewController.swift
@@ -33,11 +33,6 @@ class ViewController: UIViewController {
 
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     // setup CADisplayLink
 //    func startLoad()  {
 //    }
